e2522d5c8b90b24c2a6d32dcb76834ebb432509a
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-04-21 06:04:02 -0500

    fix(test-environment.ts): change path of service account file
    The path of the service account file was changed to "../test_cred.json"
    to reflect the correct location of the file.

--- src/test-environment.ts
+++ src/test-environment.ts
@@ -1,3 +1,3 @@
 process.env["TF_VAR_bucket_name"] = "django"
 process.env["TF_VAR_project_id"] = "chain"
-process.env["TF_VAR_service_account_file"] = "test_cred.json"
+process.env["TF_VAR_service_account_file"] = "../test_cred.json"
