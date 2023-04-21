1a2471f59f84d50f0df1e798a5d61c5b4ae8e9cb
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-04-21 13:34:03 -0500

    feat(main.ts): add LogBucketStack to create a logging bucket
    The LogBucketStack is added to create a logging bucket. This logging
    bucket is used to store logs generated by the application. The
    LogBucketStack takes in various arguments such as the project ID,
    region, zone, bucket name, retention period, analytics, and remote
    bucket details. These arguments are used to configure the logging
    bucket.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { App } from "cdktf"
 import BucketStack from "./bucket"
+import { LogBucketStack } from "./logBucketStack"
 import { argv } from "./command_options"
 
 const app = new App()
@@ -9,5 +10,25 @@ new BucketStack(app, "gcs-bucket-cdktf", {
   zone: argv.z,
   region: argv.rg,
   credentials: argv.c,
+})
+new LogBucketStack(app, `log-bucket-${argv.pi}`, {
+  provider: {
+    credentials: argv.c,
+    zone: argv.z,
+    region: argv.rg,
+    project: argv.pi,
+  },
+  bucket: {
+    retention: argv.rt,
+    analytics: argv.an,
+    project: argv.pi,
+    name: argv.lb,
+    region: argv.rg,
+  },
+  remote: {
+    credentials: argv.c,
+    bucketName: argv.bn,
+    bucketPrefix: argv.bp,
+  },
 })
 app.synth()
