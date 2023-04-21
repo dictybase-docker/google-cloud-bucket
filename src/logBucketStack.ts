c1fc5bf634fb4e50ef5619435fce4fe24f19ce91
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-04-21 13:07:44 -0500

    feat(logBucketStack.ts): add LogBucketStack class
    This commit adds a new file, logBucketStack.ts, which contains a new
    class called LogBucketStack. This class is responsible for creating a
    new Google Cloud Storage bucket for storing logs. It takes in a set of
    properties, including the provider, remote state, and bucket properties,
    and uses them to create a new LoggingProjectBucketConfig resource in the
    Terraform stack. This resource creates a new Google Cloud Storage bucket
    with the specified properties, including retention days and analytics
    settings. The class also supports using a remote state backend for
    storing the Terraform state file.

---
+++ src/logBucketStack.ts
@@ -0,0 +1,61 @@
+import { Construct } from "constructs"
+import { TerraformStack, GcsBackend } from "cdktf"
+import { readFileSync } from "fs"
+import { GoogleProvider } from "@cdktf/provider-google/lib/provider"
+import { LoggingProjectBucketConfig } from "@cdktf/provider-google/lib/logging-project-bucket-config"
+
+type ProviderProperties = {
+  credentials: string
+  zone: string
+  region: string
+  project: string
+}
+
+type RemoteStateProperties = {
+  credentials: string
+  bucketName: string
+  bucketPrefix: string
+}
+
+type LogBucketProperties = {
+  retention: number
+  analytics: boolean
+  project: string
+  name: string
+  region: string
+}
+
+type LogBucketStackProperties = {
+  provider: ProviderProperties
+  remote?: RemoteStateProperties
+  bucket: LogBucketProperties
+}
+
+class LogBucketStack extends TerraformStack {
+  constructor(scope: Construct, id: string, props: LogBucketStackProperties) {
+    super(scope, id)
+    const { provider, remote, bucket } = props
+    new GoogleProvider(this, "google", {
+      credentials: readFileSync(provider.credentials).toString(),
+      project: provider.project,
+      region: provider.region,
+      zone: provider.zone,
+    })
+    if (remote) {
+      new GcsBackend(this, {
+        bucket: remote.bucketName,
+        prefix: remote.bucketPrefix,
+        credentials: readFileSync(remote.credentials).toString(),
+      })
+    }
+    new LoggingProjectBucketConfig(this, `${id}-logging-bucket-config`, {
+      project: bucket.project,
+      location: bucket.region,
+      bucketId: bucket.name,
+      retentionDays: bucket.retention,
+      enableAnalytics: bucket.analytics,
+    })
+  }
+}
+
+export { LogBucketStack }
