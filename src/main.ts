01b83c8ccd79a5c7f2bd966da8edc5fb76427a5f
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-04-21 06:40:39 -0500

    feat(main.ts): add command line options to BucketStack constructor
    The BucketStack constructor now accepts command line options for
    bucketName, projectId, zone, region, and credentials.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { App } from "cdktf"
 import BucketStack from "./bucket"
+import { argv } from "./command_options"
 
 const app = new App()
-new BucketStack(app, "gcs-bucket-cdktf")
+new BucketStack(app, "gcs-bucket-cdktf", {
+  bucketName: argv.bn,
+  projectId: argv.pi,
+  zone: argv.z,
+  region: argv.rg,
+  credentials: argv.c,
+})
 app.synth()
